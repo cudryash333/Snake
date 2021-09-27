b464973df435620ecf8573c7534edf1355fb0e20
cudryash333 <58400682+cudryash333@users.noreply.github.com>
2021-09-27 16:21:35 +0300

    creating static tview and clear gview

--- main.cpp
+++ main.cpp
@@ -1,8 +1,59 @@
+#include <iostream>
+#include <string>
+#include "tview.h"
+#include "gview.h"
+#include "view.h"
 #include "game.h"
-using namespace std; 
+#include "controller.h"
+#include "ai_controller.h"
 
-int main() 
+using namespace std;
+using namespace std::placeholders;
+
+class Quiter
 {
-	game g;
+public:
+	Quiter(View* myv)
+	{
+		v = myv;
+		v->onkey(bind(&Quiter::key_pressed, this, _1));
+	}
+	void key_pressed(int key)
+	{
+		if (key == 'q')
+		{
+			v->quit();
+		}
+	}
+	View* v;
+};
+
+int main(int argc, char* argv[]) {
+
+	char type;
+	std::cin >> type;
+	View* v = View::create(type);
+	// Gview* v;
+	Game g(v);
+	KeyController c(&g.get_snakes().front(), v);	
+	AiController ai(v, &g.create_snake(), &g);
+	AiController ai_2(v, &g.create_snake(), &g);
+	Quiter q(v);
+	v->draw();
+	for (auto& rabbit : g.get_rabbits())
+	{
+		v->paint(rabbit.get_coord());
+	}
+	int i = 0;
+	for (auto& snake : g.get_snakes())
+	{
+		DrawSnake draw_snake;
+		draw_snake.body = snake.get_body();
+		draw_snake.direct = snake.get_direct();
+		draw_snake.color = i;
+		i++;
+		v->paint(draw_snake);
+	}
+	v->runloop();
 	return 0;
 }
